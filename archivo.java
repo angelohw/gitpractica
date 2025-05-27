86528670ba19100177431ffcf28fe26fefae34ba
angelohw <78236670+angelohw@users.noreply.github.com>
2025-05-27 02:44:39 +0200

    Update archivo.java

--- archivo.java
+++ archivo.java
@@ -2,6 +2,6 @@ public class archivo {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.println("hola mundo");
+        System.out.println("adios mundo cruelllll");
     }
 }
