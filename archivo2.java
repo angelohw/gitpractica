6a0b847d0061bf7c05f2f12a663bef062ba240a5
angelo <angelo.carp1901@gmail.com>
2025-05-27 02:31:52 +0200

    cambio para subirlo de nuevo al repo

--- archivo2.java
+++ archivo2.java
@@ -1,6 +1,6 @@
 public class archivo2 {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo como estas");
+        System.out.println("hola mundo como estas push para cambiar codigo");
     }
 }
